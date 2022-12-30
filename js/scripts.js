71a16f6b0e4860d75ca96c1f8e4a09170bbd462e
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2022-12-30 11:22:43 +0100

    add costrutto Switch-Case

--- js/scripts.js
+++ js/scripts.js
@@ -6,28 +6,58 @@ console.log('Voto:', vote);
 
 // convertiamo il voto numerico in giudizio
 
-if (vote < 18) {
+// if (vote < 18) {
 
-    console.log('Giudizio: Insufficente');
+//     console.log('Giudizio: Insufficente');
 
-} else if (vote === 18 || vote < 21) {
+// } else if (vote === 18 || vote < 21) {
 
-    console.log('Giudizio: Sufficiente');
+//     console.log('Giudizio: Sufficiente');
 
-} else if (vote === 21 || vote < 24) {
+// } else if (vote === 21 || vote < 24) {
 
-    console.log('Giudizio: Buono');
+//     console.log('Giudizio: Buono');
 
-} else if (vote === 24 || vote < 27) {
+// } else if (vote === 24 || vote < 27) {
 
-    console.log('Giudizio: Distinto');
+//     console.log('Giudizio: Distinto');
 
-} else if (vote === 27 || vote < 30) {
+// } else if (vote === 27 || vote < 30) {
 
-    console.log('Giudizio: Ottimo');
+//     console.log('Giudizio: Ottimo');
 
-} else if (vote === 30) {
+// } else if (vote === 30) {
 
-    console.log('Giudizio: Eccellente');
+//     console.log('Giudizio: Eccellente');
 
+// }
+
+switch (true) {
+
+    case vote < 18:
+        console.log('Giudizio: Insufficente');
+    break; 
+
+    case vote < 21:
+        console.log('Giudizio: Sufficiente');
+    break;
+
+    case vote < 24:
+        console.log('Giudizio: Buono');
+    break;
+
+    case vote < 27:
+        console.log('Giudizio: Distinto');
+    break;
+
+    case vote < 30:
+        console.log('Giudizio: Ottimo');
+    break;
+
+    case vote == 30:
+        console.log('Giudizio: Eccellente');
+    break;
+
+    default:
+        console.log('Valore non valido!');
 }
